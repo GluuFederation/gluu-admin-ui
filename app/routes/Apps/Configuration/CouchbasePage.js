4827d059413b15f6eedcbb446542bd550a681c80
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-03-19 22:22:53 +0600

    fix(couchbase): undefined issue

--- app/routes/Apps/Configuration/CouchbasePage.js
+++ app/routes/Apps/Configuration/CouchbasePage.js
@@ -17,15 +17,14 @@ import { getCouchBaseConfig, editCouchBase } from '../../../redux/actions/Couchb
 
 function CouchbasePage({ couch, loading, dispatch }) {
 
-  const [initialValues,setInitialValues]= useState();
+  const [initialValues, setInitialValues]= useState({});
 
   useEffect(() => {
     dispatch(getCouchBaseConfig())
-    console.log("**couch: ", couch)
   }, [])
 
   useEffect(() => {
-    if(couch){
+    if(couch.length){
       setInitialValues({
         configId: couch[0].configId,
         servers: couch[0].servers ,
@@ -46,16 +45,17 @@ function CouchbasePage({ couch, loading, dispatch }) {
           renderChildren={true}
           message={'Performing the request, please wait!'}
         >
+         
           <Card>
-            { initialValues && <CardBody>
+            { Object.keys(initialValues).length && <CardBody>
               <Formik
                 initialValues={initialValues}
                 onSubmit={(values) => {
                   dispatch(editCouchBase(JSON.stringify(values)))
                 }}
-              >
-                {
-                  (formik) => (
+                enableReinitialize
+                render={formik => {
+                  return (
                     <Form onSubmit={formik.handleSubmit}>
                       {Object.keys(couch).length ? couch.map((couchbase, index) => (
                         <CouchbaseItem
@@ -69,8 +69,8 @@ function CouchbasePage({ couch, loading, dispatch }) {
                       <GluuFooter />
                     </Form>
                   )
-                }
-
+                }}
+              >
               </Formik>
             </CardBody>}
           </Card>
